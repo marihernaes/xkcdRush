ed9ed662a242935e8220c958e93ef4fc5edec6ff
mhernaes <hernesmari@gmail.com>
2024-09-15 13:09:23 +0200

    ui: display title and date

--- app/src/main/java/com/example/xkcdrush/ui/ComicsFragment.kt
+++ app/src/main/java/com/example/xkcdrush/ui/ComicsFragment.kt
@@ -10,12 +10,19 @@ import com.example.xkcdrush.R
 import com.example.xkcdrush.databinding.FragmentComicsBinding
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 @AndroidEntryPoint
 class ComicsFragment : Fragment(R.layout.fragment_comics) {
 
     private lateinit var binding: FragmentComicsBinding
     private val viewModel: ComicsViewModel by viewModels()
+
+    private val defaultCalendar = Calendar.getInstance()
+    private val defaultTimeFormat =
+        SimpleDateFormat("EEEE, MMMM d, yyyy", Locale.ENGLISH) // Sunday, October 13, 2024
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -34,9 +41,16 @@ class ComicsFragment : Fragment(R.layout.fragment_comics) {
             viewModel.loadRandomComic()
         }
 
+        binding.loadNewestButton.setOnClickListener {
+            viewModel.loadNewestComic()
+        }
+
         viewModel.comicState.observe(viewLifecycleOwner) { comicState ->
             val imageView: ImageView = view.findViewById(R.id.imageView)
-            var altText = ""
+            var alt = ""
+            var title = ""
+            var subtitle = ""
+
             when (comicState) {
                 is ComicState.Error -> {
                     imageView.load(R.drawable.error)
@@ -49,16 +63,27 @@ class ComicsFragment : Fragment(R.layout.fragment_comics) {
                 }
 
                 is ComicState.Success -> {
-                    imageView.load(comicState.comic.img) {
-                        placeholder(R.drawable.placeholder)
-                        error(R.drawable.error)
+                    comicState.comic.let { comic ->
+                        imageView.load(comic.img) {
+                            placeholder(R.drawable.placeholder)
+                            error(R.drawable.error)
+                        }
+                        alt = comic.alt
+                        defaultCalendar.set(
+                            comic.year.toInt(),
+                            comic.month.toInt(),
+                            comic.day.toInt()
+                        )
+                        subtitle = defaultTimeFormat.format(defaultCalendar.time)
+                        title = comic.title
                     }
-                    altText = comicState.comic.alt
                     updateButtonStates()
                 }
             }
+            binding.title.text = title
+            binding.subtitle.text = subtitle
             binding.imageView.setOnLongClickListener {
-                Snackbar.make(binding.imageView, altText, Snackbar.LENGTH_LONG)
+                Snackbar.make(binding.imageView, alt, Snackbar.LENGTH_LONG)
                     .setTextMaxLines(20)
                     .show()
                 true
